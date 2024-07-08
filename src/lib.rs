a14aa877ec1a85b7f28530d56822ec07deac106a
Saman <samankittani@gmail.com>
2024-07-08 14:30:43 -0500

    add generic support

--- src/lib.rs
+++ src/lib.rs
@@ -8,18 +8,25 @@ pub fn into_jsvalue_derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let ident = input.ident;
     let ident_str = ident.to_string();
+    let generics = input.generics;
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+
+    // Adding where clause constraints for all generic types to implement serde::Serialize
+    let where_clause = match where_clause {
+        Some(where_clause) => quote! { #where_clause },
+        None => quote! {},
+    };
 
     quote!(
-    impl From<#ident> for wasm_bindgen::JsValue {
-        fn from(value: #ident) -> Self {
-            let res = serde_wasm_bindgen::to_value(&value);
-            wasm_bindgen::UnwrapThrowExt::expect_throw(
-                res,
-                &format!("failed to convert {} to a JsValue", #ident_str),
-            )
+        impl #impl_generics From<#ident #ty_generics> for wasm_bindgen::JsValue #where_clause {
+            fn from(value: #ident #ty_generics) -> Self {
+                let res = serde_wasm_bindgen::to_value(&value);
+                wasm_bindgen::UnwrapThrowExt::expect_throw(
+                    res,
+                    &format!("failed to convert {} to a JsValue", #ident_str),
+                )
+            }
         }
-    }
-
-        )
+    )
     .into()
 }
